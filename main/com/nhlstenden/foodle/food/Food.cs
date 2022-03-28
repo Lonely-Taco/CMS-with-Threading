6df3ad95898e6d32f51c41cad8c6f231d6f258aa
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-28 16:14:34 +0200

    Designer

--- main/com/nhlstenden/foodle/food/Food.cs
+++ main/com/nhlstenden/foodle/food/Food.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Foodle.main.com.nhlstenden.foodle
+namespace CMS.main.com.nhlstenden.foodle
 {
     internal class Food
     {
--- main/com/nhlstenden/foodle/pages/windows/FoodInfoWindow.cs
+++ main/com/nhlstenden/foodle/pages/windows/FoodInfoWindow.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Foodle.main.com.nhlstenden.foodle.pages.windows
+namespace CMS.main.com.nhlstenden.foodle.pages.windows
 {
     internal class FoodInfoWindow
     {

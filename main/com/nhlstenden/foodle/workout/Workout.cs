c53f7edcced6e1604d1c0a592d55dffa47635e44
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-28 17:31:38 +0200

    namespace change

--- main/com/nhlstenden/foodle/workout/Workout.cs
+++ main/com/nhlstenden/foodle/workout/Workout.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Foodle.main.com.nhlstenden.foodle
+namespace CMS.main.com.nhlstenden.foodle
 {
     internal class Workout
     {

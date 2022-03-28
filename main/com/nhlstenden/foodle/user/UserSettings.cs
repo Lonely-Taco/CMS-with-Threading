9973448b9c9c20b106a7e7251aa7f17d847ef7e9
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2022-03-28 16:53:31 +0200

    Revert "Foodle"
    This reverts commit 8219bece6d89092b4aaa35135008901fcb43d154.

--- main/com/nhlstenden/foodle/user/UserSettings.cs
+++ main/com/nhlstenden/foodle/user/UserSettings.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 
-namespace Foodle.main.com.nhlstenden.foodle
+namespace CMS.main.com.nhlstenden.foodle
 {
     internal class UserSettings
     {

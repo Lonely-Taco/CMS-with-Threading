fc8175507e537cac3e42f0ac7e55dab6acd21ddb
Christof145 <43606559+Christof145@users.noreply.github.com>
2022-03-30 16:11:26 +0200

    Added windows to workout page
    Added click listeners to buttons in workout and workoutSession

--- main/com/nhlstenden/foodle/pages/view_items/WorkoutPage.xaml.cs
+++ main/com/nhlstenden/foodle/pages/view_items/WorkoutPage.xaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -23,5 +27,60 @@ namespace CMS.main.com.nhlstenden.foodle.pages
         {
             this.InitializeComponent();
         }
+
+        private void OpenWorkoutWindow()
+        {
+            List<WeekDay> ScheduledDays = new List<WeekDay>((int)WeekDay.FRIDAY);
+            Workout workout = new Workout(DateTime.Now, DateTime.Now.AddMinutes(10), ScheduledDays, "Hell", "Hard", 400);
+            string workoutAsParam = JsonConvert.SerializeObject(workout);
+            OpenPageAsWindowAsync(typeof(windows.WorkoutWindow), workoutAsParam);
+        }
+
+        private void OpenWorkoutSessionWindow()
+        {
+            WorkoutSession workoutSession = new WorkoutSession(DateTime.Now, DateTime.Now.AddMinutes(10), 400);
+            string workoutSessionAsParam = JsonConvert.SerializeObject(workoutSession);
+            OpenPageAsWindowAsync(typeof(windows.WorkoutSessionWindow), workoutSessionAsParam);
+        }
+
+        public void AddWorkout_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWorkoutWindow();
+        }
+
+        public void OpenSession_Click(object sender, RoutedEventArgs e)
+        {
+            OpenWorkoutSessionWindow();
+        }
+
+        /// <summary>
+        /// Opens a page given the page type as a new window.
+        /// </summary>
+        /// <param name="t"></param>
+        private async void OpenPageAsWindowAsync(Type t, string param)
+        {
+            var currentAV = ApplicationView.GetForCurrentView();
+            var newAV = CoreApplication.CreateNewView();
+            await newAV.Dispatcher.RunAsync(
+                            CoreDispatcherPriority.Normal,
+                            async () =>
+                            {
+                                var newWindow = Window.Current;
+                                var newAppView = ApplicationView.GetForCurrentView();
+                                newAppView.Title = "New window";
+
+                                var frame = new Frame();
+                                frame.Navigate(t, param);
+                                newWindow.Content = frame;
+                                newWindow.Activate();
+
+                                await ApplicationViewSwitcher.TryShowAsStandaloneAsync(
+                                    newAppView.Id,
+                                    ViewSizePreference.UseMinimum,
+                                    currentAV.Id,
+                                    ViewSizePreference.UseMinimum);
+                            });
+        }
     }
+   
 }

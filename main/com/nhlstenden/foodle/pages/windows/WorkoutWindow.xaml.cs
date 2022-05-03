6f8c43330d2d39c489716c1339fa5b80d08d10b5
Christof145 <43606559+Christof145@users.noreply.github.com>
2022-05-03 12:03:49 +0200

    Add workout window button click

--- main/com/nhlstenden/foodle/pages/windows/WorkoutWindow.xaml.cs
+++ main/com/nhlstenden/foodle/pages/windows/WorkoutWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -26,5 +28,41 @@ namespace CMS.main.com.nhlstenden.foodle.pages.windows
         {
             this.InitializeComponent();
         }
+
+
+        private void CreateUser()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            String connetionString =
+               @"Data Source=DESKTOP-PIVKCNJ;Initial Catalog=Foodle;Persist Security Info=True;User ID='SQL Login';Password='SQLLogin';";
+            SqlConnection cnn = new SqlConnection(connetionString);
+            
+            string s = "Hello";
+            Binding b = new Binding();
+            b.Mode = BindingMode.OneTime;
+            b.Source = s;
+            TextBlock MyText = new TextBlock();
+            MyText.SetBinding(TextBlock.TextProperty, b);
+
+            Console.WriteLine(MyText.Text);
+
+            String sql = "INSERT INTO workout (window_color_hex, username) values('light', 'Ramon Brakels')";
+            try
+            {
+                cnn.Open();
+                SqlCommand command = new SqlCommand(sql, cnn);
+                command.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateUser();
+        }
+
     }
 }

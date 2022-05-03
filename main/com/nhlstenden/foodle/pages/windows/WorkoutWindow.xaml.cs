8b06adcd8df5798c1a53c1a291b00ce964d97b74
Christof145 <43606559+Christof145@users.noreply.github.com>
2022-05-03 20:09:45 +0200

    Add workout insert into database

--- main/com/nhlstenden/foodle/pages/windows/WorkoutWindow.xaml.cs
+++ main/com/nhlstenden/foodle/pages/windows/WorkoutWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,5 +29,49 @@ namespace CMS.main.com.nhlstenden.foodle.pages.windows
         {
             this.InitializeComponent();
         }
+
+        private async Task<Workout> SaveWorkout()
+        {
+            string workoutName = this.WorkoutNameInput.Text;
+            DateTime startDate = this.StartDateInput.Date.DateTime;
+            DateTime endDate = this.EndDateInput.Date.DateTime;
+            string workoutDescription = this.WorkoutDescriptionInput.Text;
+            int amountOfCaloriesBurned = (int) this.AmountOfCaloriesBurned.Value;
+            Workout workoutToAdd = new Workout(startDate, endDate, workoutName, workoutDescription, amountOfCaloriesBurned);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            String connetionString =
+            "Data Source=DESKTOP-PIVKCNJ;Initial Catalog=foodle;User Id=SQL Login; Password=SQLLogin";
+            SqlConnection cnn = new SqlConnection(connetionString);
+            String sql = "INSERT INTO workout (user_id, start_date, end_date, name, description, amount_of_calories_burned) values(@user_id, @startDate, @endDate, @workoutName, @workoutDescription, @amountOfCaloriesBurned)";
+
+            try
+            {
+                cnn.Open();
+                SqlCommand command = new SqlCommand(sql, cnn);
+                command.Parameters.AddWithValue("@user_id", 1);
+                command.Parameters.AddWithValue("@startDate", workoutToAdd.StartDate);
+                command.Parameters.AddWithValue("@endDate", workoutToAdd.EndDate);
+                command.Parameters.AddWithValue("@workoutName", workoutToAdd.WorkoutName);
+                command.Parameters.AddWithValue("@workoutDescription", workoutToAdd.Description);
+                command.Parameters.AddWithValue("@amountOfCaloriesBurned", workoutToAdd.ExpectedCaloriesBurned);
+                command.ExecuteNonQuery();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return workoutToAdd;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await SaveWorkout();
+        }
+
     }
+
+    
 }
